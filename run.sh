de0b9ceb38cabee7e39f6b159d0372ba5026a153
songjin321 <songjinxs@163.com>
2018-11-25 15:58:33 +0800

    delete others

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PROBLEM=headline_byte
+MODEL=transformer
+HPARAMS=transformer_prepend
+
+DATA_DIR=$HOME/t2t_data
+TMP_DIR=/tmp/t2t_datagen
+TRAIN_DIR=$HOME/t2t_train/$PROBLEM/$MODEL-$HPARAMS
+USER_DIR=$HOME/tensor2tensor/byteCup
+
+mkdir -p $DATA_DIR $TMP_DIR $TRAIN_DIR
+
+# Generate data
+t2t-datagen \
+  --data_dir=$DATA_DIR \
+  --tmp_dir=$TMP_DIR \
+  --problem=$PROBLEM
+
+# Train
+# *  If you run out of memory, add --hparams='batch_size=1024'.
+t2t-trainer \
+  --data_dir=$DATA_DIR \
+  --problem=$PROBLEM \
+  --model=$MODEL \
+  --hparams_set=$HPARAMS \
+  --output_dir=$TRAIN_DIR
+  --t2t_usr_dir=$USER_DIR
