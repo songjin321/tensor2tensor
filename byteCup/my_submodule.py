8fa08c08169d7e88f6231bdbc20430fc6eea4c3b
songjin321 <songjinxn@163.com>
2018-11-29 10:53:47 +0800

    add packed length for tpu train

--- byteCup/my_submodule.py
+++ byteCup/my_submodule.py
@@ -32,12 +32,11 @@ from tensor2tensor.models.transformer import *
 import json
 import tensorflow as tf
 
-# Links to data from http://cs.nyu.edu/~kcho/DMQA/
-BUCKET = 'bytecup2018' #@param {type:"string"}
+BUCKET = 'bytecup2018' 
 assert BUCKET, 'Must specify an existing GCS bucket name'
 TASK_DATA_DIR = 'gs://{}/bytecup2018'.format(BUCKET)
 TRAIN_DATA_PATH = os.path.join(TASK_DATA_DIR, "bytecup.corpus.train.0.txt")
-print('***** Task data directory: {} *****'.format(TASK_DATA_DIR))
+
 
 def _story_summary_split(story):
   split_str = u" <summary> "
@@ -49,8 +48,8 @@ def example_generator(all_files, sum_token):
     """Generate examples."""
     with tf.gfile.Open(all_files, "r") as f:
         lines = f.readlines()
-    for (i, line) in enumerate(lines):
-        story = json.loads(line)['content']
+    for line in lines:
+        story = json.loads(line)['content'][:512]
         summary = json.loads(line)['title']
         story_summary_split_token = u" <summary> " if sum_token else " "
         yield " ".join(story) + story_summary_split_token + " ".join(summary)
@@ -58,18 +57,12 @@ def example_generator(all_files, sum_token):
 @registry.register_problem
 class HeadlineByte(text_problems.Text2TextProblem):
   """Headline generation for byte competetion"""
-
-  def generate_text_for_vocab(self, data_dir, tmp_dir):
-    del data_dir
-    all_files = TRAIN_DATA_PATH
-    return example_generator(all_files, sum_token=False)
-
   @property
   def dataset_splits(self):
     """Splits of data to produce and number of output shards for each."""
     return [{
         "split": problem.DatasetSplit.TRAIN,
-        "shards": 1000,
+        "shards": 100,
     }, {
         "split": problem.DatasetSplit.EVAL,
         "shards": 1,
@@ -83,10 +76,20 @@ class HeadlineByte(text_problems.Text2TextProblem):
 
   def generate_samples(self, data_dir, tmp_dir, dataset_split):
     del data_dir
+    del tmp_dir
+    del dataset_split
     all_files = TRAIN_DATA_PATH
     for example in example_generator(all_files, sum_token=True):
       story, summary = _story_summary_split(example)
       yield {"inputs": story, "targets": summary}
+
+  @property
+  def packed_length(self):
+    return 256
+
+  @property
+  def max_samples_for_vocab(self):
+    return 20000
 
 @registry.register_hparams
 def transformer_headline():
