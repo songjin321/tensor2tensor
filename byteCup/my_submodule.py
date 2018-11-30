804091e3d334a96b80ed58cf1bde15caf4f93aa5
songjin321 <songjinxn@163.com>
2018-11-30 20:50:30 +0800

    4096

--- byteCup/my_submodule.py
+++ byteCup/my_submodule.py
@@ -135,5 +135,5 @@ def transformer_headline():
   hparams = transformer_big()
   hparams.prepend_mode = "prepend_inputs_masked_attention"
   update_hparams_for_tpu(hparams)
-  hparams.batch_size = 2048 * 3
+  hparams.batch_size = 2048 * 2
   return hparams
