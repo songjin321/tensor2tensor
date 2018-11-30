5eb01add70941bfff505eb47c2644dd0608381f5
songjin321 <songjinxn@163.com>
2018-11-30 14:14:58 +0800

    add batch size

--- byteCup/my_submodule.py
+++ byteCup/my_submodule.py
@@ -135,5 +135,5 @@ def transformer_headline():
   hparams = transformer_base_v2()
   hparams.prepend_mode = "prepend_inputs_masked_attention"
   update_hparams_for_tpu(hparams)
-  hparams.batch_size = 2048
+  hparams.batch_size = 4096*2
   return hparams
