23b62f939b9fc6e6d3c67ccfc69b3abad56842e8
songjin321 <songjinxn@163.com>
2018-12-01 09:29:21 +0800

    big model

--- byteCup/my_submodule.py
+++ byteCup/my_submodule.py
@@ -132,8 +132,7 @@ class HeadlineTest(HeadlineByte):
 
 @registry.register_hparams
 def transformer_headline():
-  hparams = transformer_base()
+  hparams = transformer_big()
   hparams.prepend_mode = "prepend_inputs_masked_attention"
   update_hparams_for_tpu(hparams)
-  hparams.batch_size = 2048 * 2
   return hparams
--- train.sh
+++ train.sh
@@ -6,7 +6,7 @@ HPARAMS=transformer_headline
 USER_DIR=$HOME/tensor2tensor/byteCup
 STORAGE_BUCKET=gs://bytecup2018/tensor2tensor
 DATA_DIR=$STORAGE_BUCKET/t2t_data
-TRAIN_DIR=$STORAGE_BUCKET/t2t_train/headline_byte
+TRAIN_DIR=$STORAGE_BUCKET/t2t_train/headline_byte_big
 
 # Train
 # *  If you run out of memory, add --hparams='batch_size=1024'.
@@ -17,7 +17,7 @@ t2t-trainer \
   --hparams_set=$HPARAMS \
   --output_dir=$TRAIN_DIR \
   --t2t_usr_dir=$USER_DIR \
-  --train_steps=30000 \
+  --train_steps=20000 \
   --eval_steps=100 \
   --use_tpu=True \
   --cloud_tpu_name=$TPU_NAME
